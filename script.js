caad36df9fb6cf37eb60d50b533c2ed9dd86f562
pastocasa <justo.raris@escuelacangallo.edu.ar>
2024-11-29 19:34:18 -0300

    los divs cambian el texto cuando son clickeados

---
+++ script.js
@@ -0,0 +1,15 @@
+function texto_1(){
+    document.getElementById("ambiente").innerHTML = "......Ehhhh....En realidad no puedo darte informacion, disculp&aacute.";
+}
+
+function texto_2(){
+    document.getElementById("cultura").innerHTML = "Visit&aacute el Teatro Col&oacuten. La verdad no s&eacute mas de Bs. As.Estoy atrapado en una compu desde hace mucho.";
+}
+
+function texto_3(){
+    document.getElementById("seguridad").innerHTML = "&iquest;Una denucia? Uh, suena serio. Pena que no est&eacutes en la p&aacutegina verdadera.";
+}
+
+function texto_4(){
+    document.getElementById("educacion").innerHTML = "Se me acaban los chistes. Pero, &iquest;viste profe? Cambian cosas.";
+}
